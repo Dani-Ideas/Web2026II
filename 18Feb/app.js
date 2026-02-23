00dbcec4ad925e7c40ca59a85443fb997d4d0d91
Dani-Ideas <daniideas0@gmail.com>
2026-02-23 03:45:50 +0000

    first commit

---
+++ 18Feb/app.js
@@ -0,0 +1,25 @@
+/**alaert ("hello word");
+strong 
+numerico
+console.log("Danii");
+console.log(5);
+console.log(5,6);
+console.log(5+6);
+console.log(true);
+console.log(false);
+var nombre = "dani";
+// declar la de claracion de variables con un valor inicial
+var nombre = 6;
+console.log(nombre);
+
+let dato = 456123789;
+function n (){
+    let nombre = "hola"
+    console.log(nombre)
+}
+bool */
+var nombre = "daniel";
+var  carrera = "ING compu";
+var edad = 26;
+console.log("Tu nombre y tu carrera");
+console.log("nombre: "+nombre+" "+"\n"+"carrera: "+carrera)
